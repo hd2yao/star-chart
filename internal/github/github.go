508731d9742555773e9a7760222d819b9151d29b
hd2yao <1054290355@qq.com>
2023-07-28 09:59:21 +0800

    feat(github):define some prometheus variable

--- internal/github/github.go
+++ internal/github/github.go
@@ -1,9 +1,47 @@
 package github
 
-import "errors"
+import (
+    "errors"
+
+    "github.com/prometheus/client_golang/prometheus"
+)
 
 // ErrRateLimit happens when we rate limit github API
 var ErrRateLimit = errors.New("rate limited, please try again later")
 
 // ErrGitHubAPI happens when github responds with something other than a 2xx
 var ErrGitHubAPI = errors.New("failed to talk with github api")
+
+var rateLimits = prometheus.NewCounter(prometheus.CounterOpts{
+    Namespace: "star-chart",
+    Subsystem: "github",
+    Name:      "rate_limit_hits_total",
+})
+
+var effectiveEtags = prometheus.NewCounter(prometheus.CounterOpts{
+    Namespace: "star-chart",
+    Subsystem: "github",
+    Name:      "effective_etag_users_total",
+})
+
+var tokensCount = prometheus.NewCounter(prometheus.CounterOpts{
+    Namespace: "star-chart",
+    Subsystem: "github",
+    Name:      "available_tokens",
+})
+
+var invalidatedTokens = prometheus.NewCounter(prometheus.CounterOpts{
+    Namespace: "star-chart",
+    Subsystem: "github",
+    Name:      "invalidated_tokens_total",
+})
+
+var rateLimiters = prometheus.NewCounter(prometheus.CounterOpts{
+    Namespace: "star-chart",
+    Subsystem: "github",
+    Name:      "rate_limit_remaining",
+})
+
+func init() {
+    prometheus.MustRegister(rateLimits, effectiveEtags, invalidatedTokens, tokensCount, rateLimiters)
+}
